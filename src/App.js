13bd56b214dd9d4575b9ad35d09e91763d71515e
anace-m <maldonadoanaceciliac@johndeere.com>
2021-03-08 07:50:40 -0600

    created Substyles component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: [],
+    }
+  }
+  getBeers = async () => {
+    const response = await axios.get("http://localhost:3000/origins");
+    console.log(response)
+    this.setState({
+      beers: response.data,
+    })
+  };
+  componentDidMount = () => {
+    this.getBeers();
+  }
+
+  render() {
+    const beers = this.state.beers.map((beer) => {
+      return (
+        <div>
+          <h3>Style: {beer.style}</h3>
+        </div>
+      )
+    })
+    return (
+      <div className="App">
+        <h1> BEER ENCICLOPEDIA </h1>
+        {beers}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Substyles.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Substyles extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+    render () {
+
+        return (
+            <div>
+                <h1> Substyles</h1>
+            </div>
+        )
+    }
+}
+
+export default Substyles;
