413d6df75851f3adac6af4bb6933acb9a3aaa06e
anace-m <maldonadoanaceciliac@johndeere.com>
2021-03-12 10:41:24 -0600

    Initial code to edit user profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,15 +5,27 @@ import { FormControl } from "react-bootstrap";
 import axios from "axios";
 import Alert from 'react-bootstrap/Alert';
 import { Redirect } from "react-router-dom"
+import Form from "react-bootstrap/Form"
+
 import HomePage from "./HomePage"
 
 class Profile extends Component {
     constructor(props) {
         super(props)
-        console.log(props)
+        console.log(props.loggedUser)
         this.state = {
             deletedUser: false,
+            password: this.props.loggedUser.password,
+            name: this.props.loggedUser.name,
+            email: this.props.loggedUser.email
         }
+    }
+
+    handleData = (event) => {
+        event.preventDefault();
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
 
     deleteUser = async (event) => {
@@ -29,16 +41,29 @@ class Profile extends Component {
         } console.log(this.state.loggedIn)
     }
 
+    editProfile = async (event) => {
+        event.preventDefault();
+        const userDetails = {
+            password: this.state.password,
+            name: this.state.name,
+            email: this.state.email
+          };
+          console.log(userDetails)
+        const response = await axios.put(`http://localhost:3005/user/${this.props.loggedUser.id}`, userDetails)
+          console.log(response.data)
+    }
+
     render() {
         return(
             <div className="profile">
                 <h1>Profile</h1>
+                <form>
                 <InputGroup className="mb-3">
                         <InputGroup.Prepend>
                             <InputGroup.Text id="basic-addon1">Username</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <FormControl
-                            value= {this.props.loggedUser.username}
+                        <FormControl readOnly
+                            defaultValue= {this.props.loggedUser.username}
                             aria-label="username"
                             aria-describedby="basic-addon1"
                         />
@@ -53,6 +78,7 @@ class Profile extends Component {
                             aria-label="password"
                             aria-describedby="basic-addon1"
                             type= "password"
+                            onChange={this.handleData}
                         />
                     </InputGroup>
 
@@ -64,6 +90,7 @@ class Profile extends Component {
                             value= {this.props.loggedUser.name}
                             aria-label="name"
                             aria-describedby="basic-addon1"
+                            onChange={this.handleData}
                         />
                     </InputGroup>
 
@@ -75,11 +102,13 @@ class Profile extends Component {
                             value= {this.props.loggedUser.email}
                             aria-label="email"
                             aria-describedby="basic-addon1"
+                            onChange={this.handleData}
                         />
                     </InputGroup>
+                    </form>
 
                     <InputGroup>
-                        <Button variant="outline-success">Edit</Button>
+                        <Button variant="outline-success" onClick={this.editProfile}>Edit</Button>
                         <Button variant="outline-danger" onClick={this.deleteUser}>Delete</Button>
                     </InputGroup>
 
