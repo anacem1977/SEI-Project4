a8afba19021a14f86cfd33f6630f81013f5a60c4
anace-m <maldonadoanaceciliac@johndeere.com>
2021-03-12 15:01:21 -0600

    included images in glassware component

--- src/components/Glassware.js
+++ src/components/Glassware.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import allGlasses from "./glassware.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "react-bootstrap/Image"
 
 class Glassware extends Component {
   render() {
@@ -9,20 +10,18 @@ class Glassware extends Component {
       return (
         <div>
             <h3>{item.name}</h3>
-            <p>{item.description}</p>
+            <p id={item.name}>{item.description}</p>
             <p><b>Benefits: </b>{item.benefits}</p>
-            <img src={item.image} alt={item.name} />
+            <Image src={item.image} rounded className="glassPics" alt={item.name}/>
         </div>
       );
     }) 
 
     return (
         <div className="glass">
-            <div className="glassware">
                 <h2>How to serve beer</h2>
                 <p>Scientific studies show that the shape of glassware will impact head development and retention. Why is this important? The foam created by pouring a beer acts as a net for many of the volatiles in a beer. What's a volatile? Compounds that evaporate from beer to create its aroma, such as hop oils, all kinds of yeast fermentation byproducts like alcohol, fusels and fruity esters, spices or other additions. So a glass that promotes a healthy foam head may enhance the trapping of certain volatiles. And as varying levels of head retention and presentation are desired with different styles of beers, different styles of glassware should be used accordingly.</p>
                 {glasses}
-            </div>
             <a href="#top">
                 <FontAwesomeIcon icon = "level-up-alt" href="#top" className="backToTop" size="3x" as="link"/>
             </a>
